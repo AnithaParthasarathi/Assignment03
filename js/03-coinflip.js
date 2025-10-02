784647a8740c35b90501e015aeb536e456501c4e
AnithaParthasarathi <159202933+AnithaParthasarathi@users.noreply.github.com>
2025-10-01 18:02:30 -0700

    js

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,18 +1,18 @@
 let coinFlip = Math.round(Math.random()); // 0 or 1
 let choice = prompt("Choose Heads or Tails:").toLowerCase();
 
-if (coinFlip === 0) { // heads
-    if (choice === "heads") {
+if (coinFlip == 0) { // heads
+    if (choice == "heads") {
         alert("The flip was heads and you chose heads...you win!");
-    } else if (choice === "tails") {
+    } else if (choice == "tails") {
         alert("The flip was heads but you chose tails...you lose!");
     } else {
         alert("Invalid choice. Please choose 'Heads' or 'Tails'.");
     }
 } else { // tails
-    if (choice === "tails") {
+    if (choice == "tails") {
         alert("The flip was tails and you chose tails...you win!");
-    } else if (choice === "heads") {
+    } else if (choice == "heads") {
         alert("The flip was tails but you chose heads...you lose!");
     } else {
         alert("Invalid choice. Please choose 'Heads' or 'Tails'.");
